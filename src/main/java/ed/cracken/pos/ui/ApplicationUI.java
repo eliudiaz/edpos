111caf9e0c2f360085cce16481f1665b0c358cc7
edcracken <eliudiaz@gmail.com>
2017-01-09 13:37:49 -0600

    initial project structure

---
+++ src/main/java/ed/cracken/pos/ui/ApplicationUI.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed.cracken.pos.ui;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.Responsive;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.themes.ValoTheme;
+import ed.cracken.pos.ui.app.MainScreen;
+import ed.cracken.pos.ui.authentication.AccessControl;
+import ed.cracken.pos.ui.authentication.BasicAccessControl;
+import ed.cracken.pos.ui.authentication.LoginScreen;
+import ed.cracken.pos.ui.authentication.LoginScreen.LoginListener;
+
+/**
+ *
+ * @author eliud
+ */
+@SpringUI
+@Theme("mockapp")
+public class ApplicationUI extends UI {
+
+    private AccessControl accessControl = new BasicAccessControl();
+
+    
+//    @Override
+//    protected void init(VaadinRequest request) {
+//        VerticalLayout view = new VerticalLayout();
+//        view.addComponent(new Label("Hello Vaadin!"));
+//        setContent(view);
+//    }
+    
+     @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        Responsive.makeResponsive(this);
+        setLocale(vaadinRequest.getLocale());
+        getPage().setTitle("MockApp");
+        if (!accessControl.isUserSignedIn()) {
+            setContent(new LoginScreen(accessControl, new LoginListener() {
+                @Override
+                public void loginSuccessful() {
+                    showMainView();
+                }
+            }));
+        } else {
+            showMainView();
+        }
+    }
+
+    protected void showMainView() {
+        addStyleName(ValoTheme.UI_WITH_MENU);
+        setContent(new MainScreen(ApplicationUI.this));
+        getNavigator().navigateTo(getNavigator().getState());
+    }
+
+}
