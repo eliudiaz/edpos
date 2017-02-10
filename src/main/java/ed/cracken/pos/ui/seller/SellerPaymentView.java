0b009a8d84783a9510afc72224bb86c26bb635d7
eliu diaz <eliudiaz@gmail.com>
2017-02-10 08:03:02 -0600

    payment window

--- src/main/java/ed/cracken/pos/ui/seller/SellerPaymentView.java
+++ src/main/java/ed/cracken/pos/ui/seller/SellerPaymentView.java
@@ -7,8 +7,9 @@ package ed.cracken.pos.ui.seller;
 
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
@@ -18,22 +19,44 @@ import com.vaadin.ui.Window;
  */
 public final class SellerPaymentView extends Window {
 
+    private final TextField txtCash;
+    private final TextField txtCard;
+
     public SellerPaymentView() {
-        super("Subs on Sale"); // Set window caption
+        super("Forma de pago"); // Set window caption
         center();
-
         VerticalLayout content = new VerticalLayout();
-        content.addComponent(new Label("Ingrese forma de pago:"));
-        setContent(content);
+        content.setMargin(true);
+        content.setSpacing(true);
 
+        HorizontalLayout lyCash = new HorizontalLayout();
+        HorizontalLayout lyCard = new HorizontalLayout();
+        content.addComponent(lyCash);
+        content.addComponent(lyCard);
+
+        lyCash.addComponent(new Label("Efectivo: "));
+        lyCash.setSpacing(true);
+        lyCash.addComponent(txtCash = new TextField());
+
+        lyCard.addComponent(new Label("Tarjeta: "));
+        lyCard.addComponent(txtCard = new TextField());
+        lyCard.setSpacing(true);
+
+        setContent(content);
         setClosable(false);
         setModal(true);
 
-        Button ok = new Button("OK");
+        Button ok = new Button("Aceptar");
         ok.addClickListener((ClickEvent event) -> {
-            close(); 
+            close();
         });
         content.addComponent(ok);
+
+        Button ko = new Button("Cancelar");
+        ok.addClickListener((ClickEvent event) -> {
+            close();
+        });
+        content.addComponent(ko);
     }
 
 }
