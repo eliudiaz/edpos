2aff3a8faf3fcd591459bc22b8b2bae22574d5e8
eliu diaz <eliudiaz@gmail.com>
2017-02-28 08:23:10 -0600

    product entities

--- src/main/java/ed/cracken/pos/backend/model/Category.java
+++ src/main/java/ed/cracken/pos/backend/model/Category.java
@@ -1,12 +1,19 @@
 package ed.cracken.pos.backend.model;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import javax.validation.constraints.NotNull;
 
+@Entity
 public class Category implements Serializable {
 
+    @Id
     @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @NotNull
     private String name;
