e0063dbf5d3b8636b39f18b7e22844ec73bdb0a1
eliu diaz <eliudiaz@gmail.com>
2017-02-28 08:38:43 -0600

    entities rollback to check h2 performance

--- src/main/java/ed/cracken/pos/backend/model/Category.java
+++ src/main/java/ed/cracken/pos/backend/model/Category.java
@@ -1,18 +1,16 @@
 package ed.cracken.pos.backend.model;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import javax.validation.constraints.NotNull;
 
-@Entity
+//@Entity
 public class Category implements Serializable {
 
-    @Id
-    @NotNull
+//    @Id
+//    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @NotNull
--- src/main/java/ed/cracken/pos/backend/model/Product.java
+++ src/main/java/ed/cracken/pos/backend/model/Product.java
@@ -3,21 +3,17 @@ package ed.cracken.pos.backend.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
+//@Entity
 public class Product implements Serializable {
 
     @NotNull
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id = -1;
     @NotNull
     @Size(min = 2, message = "Product name must have at least two characters")
