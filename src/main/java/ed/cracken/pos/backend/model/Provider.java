3a5f6d4c78b85b094a46fa445da8a4a282cdcb02
eliu diaz <eliudiaz@gmail.com>
2017-03-01 07:02:03 -0600

    new provider entity

--- src/main/java/ed/cracken/pos/backend/model/Provider.java
+++ src/main/java/ed/cracken/pos/backend/model/Provider.java
@@ -5,6 +5,10 @@
  */
 package ed.cracken.pos.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,8 +22,11 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @Builder
+@Entity
 public class Provider {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     private String address;
